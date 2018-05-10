17976f96c29d93e535920c743d64053a5083852f
JakeAldrich <aldrich.jake@gmail.com>
2018-05-10 03:56:21 -0400

    Project revamp
    Whole project revamp:
    -JavaScript keylisten
    -CSS Update (grid testing)
    -Displays user input and if it's correct
    -Allows user to start over
    -Pause after correct answer

--- scripts.js
+++ scripts.js
@@ -2,37 +2,64 @@ var gLtr = ["Q", "QS", "W", "WS", "E", "ES", "R", "RS", "T", "TS", "Y", "U",
   "I", "O", "OS", "P", "PS", "A", "S", "D", "F", "G", "H", "J", "K",
   "L", "Z", "X", "C", "V", "B", "N", "M"
 ];
+let ltrList = [];
+var cont = true;
 var ltrChc = "";
 var usrInp = "";
-var gmStar = false;
-var countTime = 300;
+var gameStart = false;
+var countTime;
 var tmrSp = 3000;
 var counter;
-var score = 0;
+var score;
+
+document.addEventListener("keypress", function(event) {
+  if (cont == true) {
+    console.log("log");
+    usrInp = String.fromCharCode(event.keyCode);
+    if (usrInp == 'Q')
+      usrInp = 'qs';
+    else if (usrInp == 'W')
+      usrInp = 'ws';
+    else if (usrInp == 'E')
+      usrInp = 'es';
+    else if (usrInp == 'R')
+      usrInp = 'rs';
+    else if (usrInp == 'T')
+      usrInp = 'ts';
+    else if (usrInp == 'O')
+      usrInp = 'os';
+    else if (usrInp == 'P')
+      usrInp = 'ps';
+    usrInp = usrInp.toUpperCase();
+    if (gameStart === true) {
+      checkLet();
+    }
+  }
+})
 
 function nextLet() {
+  letterListReset();
+  cont = true;
   var ran = Math.floor(Math.random() * gLtr.length);
-
   ltrChc = gLtr[ran];
-  imgName = "BigWhite/" + ltrChc + ".png";
 
-  document.getElementById('letterShow').src = imgName;
+  document.getElementById('letterShow').src = "BigWhite/" + ltrChc + ".png";
 }
 
 function checkLet() {
-  if (gmStar === true) {
-    usrInp = usrInp.toUpperCase();
-
-    //SHOW WHAT USER ENTERS
-    var arrayLength = gLtr.length;
-    var imgInp;
-    for (var i = 0; i < arrayLength; i++) {
-      if (usrInp == gLtr[i]) {
-        imgInp = "BigWhite/" + usrInp + ".png";
-        document.getElementById('usrInput').src = imgInp;
-      }
+  if (gameStart === true) {
+    if (!gLtr.includes(usrInp)) {
+      return;
     }
-    //END SHOW WHAT USER ENTERS
+
+    if (ltrList.length < 10) {
+      ltrList.push(usrInp);
+      document.getElementById('userLetters').innerHTML = ltrList;
+    } else {
+      ltrList.shift();
+      ltrList.push(usrInp.toString());
+      document.getElementById('userLetters').innerHTML = ltrList;
+    }
 
     if (usrInp == ltrChc) {
       if (countTime < 95) {
@@ -44,10 +71,15 @@ function checkLet() {
     }
     if (usrInp == ltrChc) {
       document.getElementsByClassName("score")[0].innerHTML = "SCORE: " + score;
+      document.getElementById('letterShow').src = "BigBlue/" + usrInp + ".png";
+      document.getElementById('usrInput').src = "BigBlue/" + usrInp + ".png";
+      cont = false;
+      window.setTimeout(nextLet, 800);
       timerSpeedUp();
-      nextLet();
+    } else {
+      document.getElementById('usrInput').src = "BigRed/" + usrInp + ".png";
     }
-    usrInp = "";
+
   }
 }
 
@@ -61,39 +93,34 @@ function timerSpeedUp() {
   countTime = countTime + 5;
   tmrSp = tmrSp * 1;
   counter = setInterval(timer, tmrSp);
+}
 
+function timerReset() {
+  clearInterval(counter);
+  countTime = 100;
+  tmrSp = 1000;
+  counter = setInterval(timer, tmrSp);
+}
+
+function letterListReset() {
+  ltrList = [];
+  document.getElementById('userLetters').innerHTML = ltrList;
 }
 
 function timer() {
-
   countTime = countTime - 1;
   if (countTime < 0) {
-    gmStar = false;
-    console.log(gmStar);
+    gameStart = false;
     clearInterval(counter);
     return;
   }
   document.getElementById("timerShow").innerHTML = "Time: " + countTime;
 }
 
-
 function startGame() {
-  if (gmStar === false) {
-    scoreReset();
-    gmStar = true;
-    clearInterval(counter);
-    countTime = 100;
-    tmrSp = 1000;
-    counter = setInterval(timer, tmrSp);
-    gmStar = true;
-    nextLet();
-    $(document).keypress(function(e) {
-      if (e.keyCode == e.keyCode && e.shiftKey ? 1 : 0)
-        usrInp = String.fromCharCode(e.keyCode) + "S";
-      else
-        usrInp = String.fromCharCode(e.keyCode);
-      checkLet();
-    });
-  }
-
+  gameStart = true;
+  letterListReset();
+  scoreReset();
+  timerReset();
+  nextLet();
 }
